8c3ca9c602c79d9f5a4abb4573ed505cc6aab154
bigdata67 <bigdata167@gmail.com>
2019-01-30 12:14:46 +0100

    Pipeline V1

--- src/main/java/com/formation/HelloServlet.java
+++ src/main/java/com/formation/HelloServlet.java
@@ -16,7 +16,7 @@ public class HelloServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		PrintWriter writer = response.getWriter();
-		  writer.println("Hello from HelloServlet!");
+		  writer.println("Hello Toi ! from HelloServlet!");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
